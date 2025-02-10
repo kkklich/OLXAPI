04b39b412cc0f6148a023221b4e9d34a9708abf0
krzysztof klich <krzysztof.klich@idealogic.pl>
2025-02-10 17:21:47 +0100

    get all offers

--- AF_mobile_web_api/DTO/QueryData.cs
+++ AF_mobile_web_api/DTO/QueryData.cs
@@ -4,7 +4,7 @@
     public class QueryData
     {
         public List<Data> Data { get; set; }
-        //public Metadata Metadata { get; set; }
+        public Metadata metadata { get; set; }
         //public Links Links { get; set; }
 
 
@@ -20,7 +20,7 @@
         public string Url { get; set; }
         public string Title { get; set; }
         //public DateTime LastRefreshTime { get; set; }
-        //public DateTime CreatedTime { get; set; }
+        public DateTime created_time { get; set; }
         //public DateTime ValidToTime { get; set; }
         //public object PushupTime { get; set; }
         //public string Description { get; set; }
@@ -42,15 +42,15 @@
         //public string OfferType { get; set; }
     }
 
-    //public class Promotion
-    //{
-    //    public bool Highlighted { get; set; }
-    //    public bool Urgent { get; set; }
-    //    public bool TopAd { get; set; }
-    //    public List<object> Options { get; set; }
-    //    public bool B2cAdPage { get; set; }
-    //    public bool PremiumAdPage { get; set; }
-    //}
+    public class Promotion
+    {
+        public bool Highlighted { get; set; }
+        public bool Urgent { get; set; }
+        public bool TopAd { get; set; }
+        public List<object> Options { get; set; }
+        public bool B2cAdPage { get; set; }
+        public bool PremiumAdPage { get; set; }
+    }
 
     public class Param
     {
@@ -172,8 +172,8 @@
 
     public class Metadata
     {
-        public int TotalElements { get; set; }
-        public int VisibleTotalCount { get; set; }
+        public int total_elements { get; set; }
+        public int visible_total_count { get; set; }        
         public List<object> Promoted { get; set; }
         public string SearchId { get; set; }
         public Adverts Adverts { get; set; }
--- AF_mobile_web_api/Services/RealEstateServices.cs
+++ AF_mobile_web_api/Services/RealEstateServices.cs
@@ -7,18 +7,43 @@ namespace OLX_web_api.Services
     public class RealEstateServices
     {
         private readonly HTTPClientServices _httpClient;
-        //public RealEstateServices()
-        //{
+       
         public RealEstateServices(HTTPClientServices httpClient)
         {
             _httpClient = httpClient;
         }
 
-        public async Task<QueryData> GetDefaultResponse(int offset = 0, int limit = 40)
+
+        //public static async Task<string> FetchOlxOffers(int offset = 0, int limit = 40, int categoryId = 14, int regionId = 4, int cityId = 8959, int priceFrom = 50000, int priceTo = 250000)
+        //{
+        //    string url = $"https://www.olx.pl/api/v1/offers/?offset=" +
+        //        $"{offset}&limit={limit}&category_id={categoryId}&region_id={regionId}&city_id={cityId}&filter_float_price%3Afrom={priceFrom}&filter_float_price%3Ato={priceTo}";
+
+
+
+        public async Task<QueryData> GetDefaultResponse(int offset = 0, int limit = 40, int priceFrom = 50000, int priceTo = 250000)
         {
+            int categoryId = 14;
+            int regionId = 4;
+            int cityId = 8959;
+
             var realEstateQuery = ConstantHelper.OLXAPI;
             
-            var queryParams = $"?offset={offset}&limit={limit}&category_id={ConstantHelper.RealEstateCategory}";
+            var queryParams = $"?offset={offset}" +
+                $"&limit={limit}" +
+                $"&category_id={categoryId}" +
+                $"&region_id={regionId}" +
+                $"&city_id={cityId}" +
+                $"&filter_float_price%3Afrom={priceFrom}" +
+                $"&filter_float_price%3Ato={priceTo}";
+
+
+
+            //var queryParams = $"?offset={offset}" +
+            //    $"&limit={limit}" +
+            //    $"&category_id={ConstantHelper.RealEstateCategory}" +
+            //    $"&region_id={4}" +
+            //    $"&city_id={8959}";
             realEstateQuery += queryParams;
 
             var rawResponse = await _httpClient.GetRaw(realEstateQuery);
@@ -34,12 +59,41 @@ namespace OLX_web_api.Services
         public async Task<QueryData> GetMoreResponse()
         {
             QueryData query = new QueryData();
-            for (int i = 0; i < 2; i++)
+            int limit = 40;
+            int totalQuantityResult = 0;
+            for (int j = 0; j < 4; j++)
             {
-                var response = await GetDefaultResponse(i * 40, 40);
-                query.Data.AddRange(response.Data);
+                for (int i = 0; i < 25; i++)
+                {
+                    var response = await GetDefaultResponse(i * limit, limit, (200000 * j) + 50000, (200000* j) + 250000);
+                    query.Data.AddRange(response.Data);
+
+                    await Task.Delay(60);
+                    totalQuantityResult = response.metadata.visible_total_count;
+                }
+                var xd = totalQuantityResult;
             }
 
+            query.Data = query.Data.GroupBy(o => o.Id).Select(g => g.First()).OrderBy(x => x.created_time).ToList();
+
+            query.metadata = new Metadata();
+            query.metadata.visible_total_count = totalQuantityResult;
+            query.metadata.total_elements = query.Data.Count;
+
+            //int i = 0;
+            //int executedQuery = 0;
+            //do
+            //{
+            //    var response = await GetDefaultResponse(i * limit, limit);
+            //    query.Data.AddRange(response.Data);
+
+            //    await Task.Delay(500);
+            //    totalQuantityResult = response.metadata.visible_total_count;
+            //    //executedQuery += response.Data.Count;
+            //    i ++;
+            //} while (query.Data.Count < totalQuantityResult);
+
+         
             return query;
         }
     }
