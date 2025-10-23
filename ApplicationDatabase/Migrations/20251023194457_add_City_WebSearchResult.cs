81289abaad422de5f99cce5ef6f3f865b7606d66
krzysztof <krzysztof.klich@idealogic.pl>
2025-10-23 22:07:30 +0200

    add city name to WebSearchResults

---
+++ ApplicationDatabase/Migrations/20251023194457_add_City_WebSearchResult.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicationDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_City_WebSearchResult : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "WebSearchResults",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "WebSearchResults");
+        }
+    }
+}
